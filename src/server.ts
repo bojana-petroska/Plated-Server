68d7ea387adbf42cf67ff3ece69cbc4c810d8216
bojana-petroska <bojana.petroska@yahoo.com>
2024-09-28 16:16:20 +0200

    set up user controllers & routes for CRUD

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
+import router from './routes/userRoutes.js';
 
 const app = express();
 const port = 5001;
 
-app.get('/', (req, res) => {
-  console.log(`server started`);
-});
+app.use(express.json());
+app.use('/users', router);
 
 app.listen(port, () => {
-  console.log(`server is running on http://localhost:${port}`);
+  console.log(`server is running on http://localhost:${port}/users`);
 });
---
+++ src/types/userTypes.ts
@@ -0,0 +1,13 @@
+export interface IUser {
+  id: number;
+  userName: string;
+  email: string;
+  password: string;
+  address: string;
+  phoneNumber: string;
+  orderHistory?: Array<string>;
+  role?: string;
+  createdAt?: Date;
+}
+
+export type UserInput = Pick<IUser, 'userName' | 'email' | 'password'>;
