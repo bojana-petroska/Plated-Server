491a7ae0077dd0b966aef69d7e89d803791a15bb
bojana-petroska <bojana.petroska@yahoo.com>
2024-09-29 14:53:54 +0200

    cors added

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import userRouter from './routes/userRoutes.js';
 import restaurantRouter from './routes/restaurantRoutes.js';
 import menuItemRouter from './routes/menuItemRoutes.js';
@@ -6,7 +7,9 @@ import menuItemRouter from './routes/menuItemRoutes.js';
 const app = express();
 const port = 5001;
 
+app.use(cors());
 app.use(express.json());
+
 app.use('/users', userRouter);
 app.use('/restaurants', restaurantRouter);
 app.use('/menu-items', menuItemRouter);
