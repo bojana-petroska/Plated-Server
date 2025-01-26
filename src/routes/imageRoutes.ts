73239324fc57be7c3d0d7a4922b96cb4615fc663
bojana-petroska <bojana.petroska@yahoo.com>
2025-01-26 10:04:25 +0100

    remove imageRoutes

--- src/routes/imageRoutes.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import multer from '../config/multerConfig.js';
-import cloudinary from '../config/cloudinaryConfig.js';
-
-const router = express.Router();
-
-export default router;
